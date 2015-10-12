8d646c4772b4755aaebb5452ca4ee311f38d0ad0
Daniel Bevenius <daniel.bevenius@gmail.com>
2015-10-12 07:04:08 +0200

    Changing to use Mocha and Chai for tests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,15 @@
 module.exports = function(grunt) {
   'use strict';
-
   grunt.initConfig({
-    _test_runner: 'whiskey',
-    _unit_tests: [
-      'test/test_fh_logger.js'
-    ],
-    _unit_args: '--real-time --report-timing --failfast --timeout 50000 --tests "<%= _unit_tests.join(\' \') %>"',
-
     // These are the properties that grunt-fh-build will use
-    unit: '<%= _test_runner %> <%= _unit_args %>',
-    unit_cover: 'istanbul cover --dir cov-unit <%= _test_runner %> -- <%= _unit_args %>'
+    //unit: '<%= _test_runner %> <%= _unit_args %>',
+    //unit_cover: 'istanbul cover --dir cov-unit <%= _test_runner %> -- <%= _unit_args %>',
+    mochaTest: {
+      all: ['./test/*.js']
+    }
   });
-
+  grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-fh-build');
   grunt.registerTask('default', ['fh:default']);
+  grunt.registerTask('default', ['mochaTest', 'fh:default']);
 };
--- test/test_fh_logger.js
+++ test/test_fh_logger.js
@@ -1,145 +1,229 @@
-var fs = require('fs');
 var fh_logger = require('../lib/fh_logger.js');
-/* only using bunyan's contants for levels in assertions */
+var expect = require('chai').expect
 var bunyan = require('bunyan'); 
+var fs = require('fs');
 
-exports.test_createLogger_simple = function (test, assert) {
-  var config = {name: 'simple'};
-  var logger = fh_logger.createLogger(config);
-  assert.strictEqual(logger.fields.name, 'simple');
-  assert.isDefined(logger.serializers);
-  assert.strictEqual(logger.streams[0].type, 'stream');
-  assert.strictEqual(logger.streams[0].level, bunyan.INFO);
-  assert.strictEqual(logger.streams[0].stream, process.stdout);
-  test.finish();
-}
+describe('fh_logger.createLogger', function() {
 
-exports.test_createLogger_config_from_json = function (test, assert) {
-  var logFile = './test/test_fh_logger.log';
-  var config = {name: 'from_json', streams: [
-    {type: 'file', stream: 'file', path: logFile, level: 'info'}
-  ]};
-  var logger = fh_logger.createLogger(config);
-  assert.strictEqual(logger.fields.name, 'from_json');
-  assert.isDefined(logger.serializers);
-  assert.strictEqual(logger.streams.length, 1);
-  assert.strictEqual(logger.streams[0].type, 'file');
-  assert.strictEqual(logger.streams[0].level, bunyan.INFO);
-  assert.strictEqual(logger.streams[0].path, logFile);
-  test.finish();
-}
-
-exports.test_createLogger_config_from_string = function (test, assert) {
-  var logFile = './test/test_fh_logger.log';
-  var config = JSON.stringify({name: 'from_string', streams: [
-    {type: 'file', stream: 'file', path: logFile, level: 'info'}
-  ]});
-  var logger = fh_logger.createLogger(config);
-  assert.strictEqual(logger.fields.name, 'from_string');
-  assert.isDefined(logger.serializers);
-  assert.strictEqual(logger.streams.length, 1);
-  assert.strictEqual(logger.streams[0].type, 'file');
-  assert.strictEqual(logger.streams[0].level, bunyan.INFO);
-  assert.strictEqual(logger.streams[0].path, logFile);
-
-  logger.info("Dummy log statement");
-  fs.readFile(logFile, 'utf-8', function(err, data) {
-    assert.ifError(err);
-    assert.ok(data);
-    deleteFile(logFile);
-    test.finish();
-  });
-}
-
-exports.test_createLogger_config_from_string_stdout = function (test, assert) {
-  var logFile = './test/test_fh_logger.log';
-  var config = JSON.stringify({name: 'from_string_sdout', streams: [
-    {type: 'stream', stream: 'process.stdout', level: 'debug'}
-  ]});
-  var logger = fh_logger.createLogger(config);
-  assert.strictEqual(logger.fields.name, 'from_string_sdout');
-  assert.isDefined(logger.serializers);
-  assert.strictEqual(logger.streams.length, 1);
-  assert.strictEqual(logger.streams[0].type, 'stream');
-  assert.strictEqual(logger.streams[0].level, bunyan.DEBUG);
-  logger.info("Dummy log statement");
-  test.finish();
-}
-
-exports.test_createLogger_config_from_string_raw_ringbuffer = function (test, assert) {
-  var config = JSON.stringify({name: 'from_string_raw', streams: [
-    {type: 'raw', stream: 'ringbuffer', level: 'trace'}
-  ]});
-  var logger = fh_logger.createLogger(config);
-  assert.strictEqual(logger.fields.name, 'from_string_raw');
-  assert.isDefined(logger.serializers);
-  assert.strictEqual(logger.streams.length, 1);
-  assert.strictEqual(logger.streams[0].type, 'raw');
-  assert.strictEqual(logger.streams[0].level, bunyan.TRACE);
-  logger.trace("Raw trace log statement");
-  var record = logger.streams[0].stream.records[0];
-  assert.strictEqual(record.name, 'from_string_raw');
-  assert.strictEqual(record.msg, 'Raw trace log statement');
-  test.finish();
-}
-
-exports.test_createLogger_stream_process_stdout = function (test, assert) {
-  var config = {name: 'test_stdout', streams: [{stream: 'process.stdout', type: 'stream'}]};
-  var logger = fh_logger.createLogger(JSON.stringify(config));
-  assert.strictEqual(logger.fields.name, 'test_stdout');
-  assert.isDefined(logger.serializers);
-  assert.strictEqual(logger.streams.length, 1);
-  assert.strictEqual(logger.streams[0].type, 'stream');
-  assert.strictEqual(logger.streams[0].level, bunyan.INFO);
-  assert.strictEqual(logger.streams[0].stream, process.stdout);
-  test.finish();
-}
-
-exports.test_createLogger_stream_process_stderr = function (test, assert) {
-  var config = {name: 'test_stderr', streams: [{stream: 'process.stderr', type: 'stream'}]};
-  var logger = fh_logger.createLogger(JSON.stringify(config));
-  assert.strictEqual(logger.fields.name, 'test_stderr');
-  assert.isDefined(logger.serializers);
-  assert.strictEqual(logger.streams.length, 1);
-  assert.strictEqual(logger.streams[0].stream, process.stderr);
-  assert.strictEqual(logger.streams[0].level, bunyan.INFO);
-  test.finish();
-}
-
-exports.test_createLogger_config_from_file = function (test, assert) {
-  fs.readFile(__dirname + '/config.json', 'utf-8', function(err, config) {
-    assert.ifError(err);
-    var logger = fh_logger.createLogger(config);
-    assert.strictEqual(logger.fields.name, 'testing');
-    assert.isDefined(logger.serializers);
-    assert.strictEqual(logger.streams.length, 3);
-    assert.strictEqual(logger.streams[0].type, 'file');
-    assert.strictEqual(logger.streams[0].level, bunyan.INFO);
-    assert.isDefined(logger.streams[0].path);
-    assert.strictEqual(logger.streams[1].type, 'stream');
-    assert.strictEqual(logger.streams[1].level, bunyan.DEBUG);
-    assert.strictEqual(logger.streams[1].stream, process.stdout);
-    assert.strictEqual(logger.streams[2].type, 'raw');
-    assert.strictEqual(logger.streams[2].level, bunyan.TRACE);
-
-    logger.info("From file config");
-    fs.readFile(logger.streams[0].path, 'utf-8', function(err, data) {
-      assert.ifError(err);
-      assert.ok(data);
-      deleteFile(logger.streams[0].path);
-      test.finish();
+    describe('with Bunyan defaults', function() {
+      var logger;
+      before(function(){
+        logger = fh_logger.createLogger({name: 'simple'});
+      })
+      it('name should have been set to simple', function() {
+        expect(logger.fields.name).to.equal('simple');
+      });
+      it('serializers should have be set', function() {
+        expect(logger.serializers).to.be.defined;
+      });
+      it('type should be stream', function() {
+        expect(logger.streams[0].type).to.equal('stream');
+      });
+      it('log level should be INFO', function() {
+        expect(logger.streams[0].level).to.equal(bunyan.INFO);
+      });
+      it('stream should be process.stdout', function() {
+        expect(logger.streams[0].stream).to.equal(process.stdout);
+      });
     });
-  });
-}
+
+    describe('using file type', function() {
+      var logFile = './test/test_fh_logger.log';
+      var logger;
+      before(function(){
+        var config = {name: 'file_logger', streams: [
+          {type: 'file', stream: 'file', path: logFile, level: 'info'}
+        ]};
+        logger = fh_logger.createLogger(config);
+      })
+      it('name should have been set to file_logger', function() {
+        expect(logger.fields.name).to.equal('file_logger');
+      });
+      it('serializers should have be set', function() {
+        expect(logger.serializers).to.be.defined;
+      });
+      it('type should be file', function() {
+        expect(logger.streams[0].type).to.equal('file');
+      });
+      it('log level should be INFO', function() {
+        expect(logger.streams[0].level).to.equal(bunyan.INFO);
+      });
+      it('path should be '  + logFile, function() {
+        expect(logger.streams[0].path).to.equal(logFile);
+      });
+    });
+
+    describe('using file type from string configuration', function() {
+      var logFile = './test/test_fh_logger_string.log';
+      var logger;
+      before(function(){
+        var config = JSON.stringify({name: 'file_logger_string', streams: [
+          {type: 'file', stream: 'file', path: logFile, level: 'debug'}
+        ]});
+        logger = fh_logger.createLogger(config);
+      })
+      it('name should have been set to file_logger_string', function() {
+        expect(logger.fields.name).to.equal('file_logger_string');
+      });
+      it('serializers should have be set', function() {
+        expect(logger.serializers).to.be.defined;
+      });
+      it('type should be file', function() {
+        expect(logger.streams[0].type).to.equal('file');
+      });
+      it('log level should be DEBUG', function() {
+        expect(logger.streams[0].level).to.equal(bunyan.DEBUG);
+      });
+      it('path should be '  + logFile, function() {
+        expect(logger.streams[0].path).to.equal(logFile);
+      });
+      it('logger.debug should generate entry in logFile'  + logFile, function() {
+        logger.debug("Dummy log statement");
+        fs.readFile(logFile, 'utf-8', function(err, data) {
+          expect(err).to.be.null;
+          expect(data).to.be.defined;
+          deleteFile(logFile);
+        });
+      });
+    });
+
+    describe('using process.stdout from string configuration', function() {
+      var logger;
+      before(function(){
+        var config = JSON.stringify({name: 'from_string_stdout', streams: [
+          {type: 'stream', stream: 'process.stdout', level: 'debug'}
+        ]});
+        logger = fh_logger.createLogger(config);
+      })
+      it('name should have been set to from_string_stdout', function() {
+        expect(logger.fields.name).to.equal('from_string_stdout');
+      });
+      it('serializers should have be set', function() {
+        expect(logger.serializers).to.be.defined;
+      });
+      it('type should be stream', function() {
+        expect(logger.streams[0].type).to.equal('stream');
+      });
+      it('stream should be process.stdout', function() {
+        expect(logger.streams[0].stream).to.equal(process.stdout);
+      });
+      it('log level should be DEBUG', function() {
+        expect(logger.streams[0].level).to.equal(bunyan.DEBUG);
+      });
+    });
+
+    describe('using raw ringbuffer.stdout from string configuration', function() {
+      var logger;
+      before(function(){
+        var config = JSON.stringify({name: 'from_string_raw', streams: [
+          {type: 'raw', stream: 'ringbuffer', level: 'trace'}
+        ]});
+        logger = fh_logger.createLogger(config);
+      })
+      it('name should have been set to from_string_raw', function() {
+        expect(logger.fields.name).to.equal('from_string_raw');
+      });
+      it('serializers should have be set', function() {
+        expect(logger.serializers).to.be.defined;
+      });
+      it('type should be raw', function() {
+        expect(logger.streams[0].type).to.equal('raw');
+      });
+      it('log level should be TRACE', function() {
+        expect(logger.streams[0].level).to.equal(bunyan.TRACE);
+      });
+      it('logger.trace should generate a log record', function() {
+        logger.trace("Raw trace log statement");
+        var record = logger.streams[0].stream.records[0];
+        expect(record.name).to.be.equal("from_string_raw");
+        expect(record.msg).to.be.equal("Raw trace log statement");
+      });
+    });
+
+    describe('using raw ringbuffer.stdout from string configuration', function() {
+      var logger;
+      before(function(){
+        var config = JSON.stringify({name: 'test_stderr', streams: [{
+          stream: 'process.stderr', type: 'stream'}
+        ]});
+        logger = fh_logger.createLogger(config);
+      })
+      it('name should have been set to test_stderr', function() {
+        expect(logger.fields.name).to.equal('test_stderr');
+      });
+      it('serializers should have be set', function() {
+        expect(logger.serializers).to.be.defined;
+      });
+      it('type should be stream', function() {
+        expect(logger.streams[0].type).to.equal('stream');
+      });
+      it('stream should be process.stderr', function() {
+        expect(logger.streams[0].stream).to.be.equal(process.stderr);
+      });
+      it('log level should be INFO', function() {
+        expect(logger.streams[0].level).to.equal(bunyan.INFO);
+      });
+    });
+
+    describe('from ./test/config.json', function() {
+      var logger;
+      before(function(){
+        var config = fs.readFileSync(__dirname + '/config.json').toString();
+        logger = fh_logger.createLogger(config);
+      })
+      it('name should have been set to test_stderr', function() {
+        expect(logger.fields.name).to.equal('testing');
+      });
+      it('serializers should have be set', function() {
+        expect(logger.serializers).to.be.defined;
+      });
+      it('should have three streams', function() {
+        expect(logger.streams.length).to.equal(3);
+      });
+      it('first stream should be of type file', function() {
+        expect(logger.streams[0].type).to.equal('file');
+      });
+      it('first stream level should be DEBUG', function() {
+        expect(logger.streams[0].level).to.equal(bunyan.DEBUG);
+      });
+      it('first stream path should be ./test/test.log', function() {
+        expect(logger.streams[0].path).to.equal('./test/test.log');
+      });
+      it("logging at DEBUG level should generate entry in ./test/test.log", function() {
+        logger.debug("From file config");
+        fs.readFile(logger.streams[0].path, 'utf-8', function(err, data) {
+          expect(err).to.be.null;
+          expect(data).to.be.defined;
+          deleteFile(logger.streams[0].path);
+        });
+      });
+      it('second stream should be of type stream', function() {
+        expect(logger.streams[1].type).to.equal('stream');
+      });
+      it('second stream level should be DEBUG', function() {
+        expect(logger.streams[1].level).to.equal(bunyan.DEBUG);
+      });
+      it('second stream should be process.stdout', function() {
+        expect(logger.streams[1].stream).to.equal(process.stdout);
+      });
+      it('third stream should be of type raw', function() {
+        expect(logger.streams[2].type).to.equal('raw');
+      });
+      it('third stream level should be TRACE', function() {
+        expect(logger.streams[2].level).to.equal(bunyan.TRACE);
+      });
+    });
+});
 
 var deleteFile = function(file) {
   fs.exists(file, function(exists) {
     if (exists) {
       fs.unlink(file, function(err) {
-        if (err.code != 'ENOENT') {
+        if (err && err.code != 'ENOENT') {
           throw err;
         }
       });
     }
   });
 }
+
