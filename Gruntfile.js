20b467b6065ddaa6f3d2f9ef222b7b7fea80244a
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-01-12 07:30:46 +0100

    Adding istanbul code coverage.
    Motivation:
    Currently there is no code coverage created when unit tests are run.
    This commit adds istanbul to correct this.
    
    Modifications:
    Updated Gruntfile with istanbul settings and added an exclude for the
    cov-unit directory in .gitignore.
    
    Result:
    Running the default build will generate a coverage report:
    $ grunt
    $ open cov-unit/lcov-report/index.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,6 +17,11 @@
 module.exports = function(grunt) {
   'use strict';
   grunt.initConfig({
+    _test_runner: '_mocha',
+    _istanbul: 'istanbul cover --dir',
+    _unit_args: '--recursive -t 10000 ./test/',
+    unit: '<%= _test_runner %> <%= _unit_args %>',
+    unit_cover: '<%= _istanbul %> cov-unit <%= _test_runner %> -- <%= _unit_args %>',
     mochaTest: {
       all: ['./test/*.js']
     }
